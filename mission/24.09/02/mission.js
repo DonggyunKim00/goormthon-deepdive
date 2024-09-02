cb9644c05c400758eaaf4a358ebd8d5b0e8eb9dc
donggyun <donggyunkim12@gmail.com>
2024-09-02 13:47:25 +0900

    task: (JS) 변수, 타입, 연산자 활용하기 과제

---
+++ mission/24.09/02/mission.js
@@ -0,0 +1,29 @@
+// 변수 선언과 데이터 타입 확인, 다양한 연산자 사용 예제를 포함한 자바스크립트 코드 파일, 결과 스크린샷
+
+let data;
+
+// 원시타입(Primitive Type)
+data = 'hello';
+console.log(typeof data);
+
+data = 123;
+console.log(typeof data);
+
+data = true;
+console.log(typeof data);
+
+data = null;
+console.log(typeof data);
+
+data = undefined;
+console.log(typeof data);
+
+data = Symbol();
+console.log(typeof data);
+
+// 참조 타입(Object Type)
+data = { name: 'donggyun' };
+console.log(typeof data);
+
+data = [1, 2, 3, 4, 5];
+console.log(typeof data);
